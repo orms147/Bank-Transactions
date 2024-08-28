c4bba81ad56419e8ac1c952fae31f1a0b7abb0ea
Nguyen Ha Bach <orms147@gmail.com>
2024-08-28 10:28:36 +0700

    Add project files.

---
+++ Bank Transactions/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank_Transactions.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public string AccountNumber { get; set; }
+        public string BeneficiaryName { get; set; }
+        public string BankName { get; set; }
+    }
+}
