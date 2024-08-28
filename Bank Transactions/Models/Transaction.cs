021ae74a604071b5f7bf42a162b2e0cd6eb9733f
Nguyen Ha Bach <orms147@gmail.com>
2024-08-28 14:24:40 +0700

    connect db

--- Bank Transactions/Models/Transaction.cs
+++ Bank Transactions/Models/Transaction.cs
@@ -1,10 +1,25 @@
-﻿namespace Bank_Transactions.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank_Transactions.Models
 {
     public class Transaction
     {
+        [Key]
         public int TransactionId { get; set; }
+
+        [Column(TypeName ="nvarchar(12)")]
         public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(120)")]
         public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(120)")]
         public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCode { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- Bank Transactions/Program.cs
+++ Bank Transactions/Program.cs
@@ -1,7 +1,15 @@
+using Bank_Transactions.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DI for Dbcontex
+builder.Services.AddDbContext<TransactionDbContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
+    );
 
 var app = builder.Build();
 
